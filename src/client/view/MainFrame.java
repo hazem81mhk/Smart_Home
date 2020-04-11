2c8be6ed540e7dc2183056e0ebdabf50fe7aa2b3
hazem <hazem81mhk@yahoo.com>
2020-04-12 00:26:24 +0200

    Initiate the class with important command

--- src/client/view/MainFrame.java
+++ src/client/view/MainFrame.java
@@ -5,11 +5,32 @@ package client.view;
  * @Mohammed Hazem Kudaimi
  */
 
+import client.controller.Controller;
+
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
     private int width=300;
-    private int hight=700;
+    private int height=700;
 
+    private Controller controller;
     private MainPanel mainPanel;
+
+    public MainFrame(){
+        this.controller=controller;
+        setupFrame();
+    }
+
+    private void setupFrame() {
+        final int offsetX = width / 5;
+        final int offsetY = height / 5;
+        setSize(width, height);
+        setTitle("<<<< Smart Home >>>>");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocation(offsetX, offsetY);
+        mainPanel = new MainPanel();
+        setContentPane(mainPanel);
+        pack();
+        setVisible(true);
+    }
 }
