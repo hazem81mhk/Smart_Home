c663c68e88789f20df694146c4b255ef26ded8f6
momo9393 <i.can.2028.tcc@gmail.com>
2020-04-30 23:57:39 +0200

    Revert "Revert "Ändringar i gui""
    This reverts commit 97e2d1195431b0b25796313a4c5af7963af9f462.

--- src/client/controller/Controller.java
+++ src/client/controller/Controller.java
@@ -1,11 +1,10 @@
 package client.controller;
 
 import client.model.Client;
-import client.view.ButtonType;
-import client.view.ClientLogin;
-import client.view.MainFrame;
-import client.view.Schedule;
+import client.view.*;
 import server.model.MainServer.ConsumptionCounter;
+import server.model.MainServer.CurtainSchedule;
+import server.model.MainServer.TempSchedule;
 
 /**
  * 11/04/2020
@@ -18,6 +17,8 @@ public class Controller {
     private ClientLogin clientLogin;
     private Client client;
     private Schedule schedule;
+    private CurtainSchedule Cschedule;
+    private TempSchedule Tschedule;
 
     public Controller() {
         clientLogin = new ClientLogin(this);
@@ -86,7 +87,63 @@ public class Controller {
             case get_consumption:
                 getConsumption();
                 break;
+
+                //AGON
+            case curtain_up:
+                client.sendRequest("up");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case curtain_down:
+                client.sendRequest("down");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case curtain_stop:
+                client.sendRequest("stop");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case curtain_schedule:
+                CSchedule();
+                break;
+            case temp_curtain:
+                CTempSchedule();
+                break;
         }
+    }
+
+    public void CButtonup() {
+        mainFrame.getMainPanel().getCurtainGui().setCurtainButtonUP();}
+
+    public void CButtondown() {
+        mainFrame.getMainPanel().getCurtainGui().setCurtainButtonDOWN();}
+
+    public void CButtonstop() {
+        mainFrame.getMainPanel().getCurtainGui().setCurtainButtonSTOP();}
+
+    public void CSchedule() {
+        mainFrame.getMainPanel().getCurtainGui().setCurtainschedulefrom();
+        mainFrame.getMainPanel().getCurtainGui().setCurtainscheduleto();
+        System.out.println(mainFrame.getMainPanel().getCurtainGui().getCurtainschedulefrom());
+       // CurtainSchedule Cschedulee = new CurtainSchedule(mainFrame.getMainPanel().getCurtainGui().getCurtainschedulefrom(),
+        //mainFrame.getMainPanel().getCurtainGui().getCurtainscheduleto());
+        //client.sendCurtainSchedule(Cschedulee);
+    }
+
+    public void CTempSchedule(){
+        mainFrame.getMainPanel().getCurtainGui().setTempschedule();
+        //TempSchedule Tschedule = new TempSchedule(mainFrame.getMainPanel().getCurtainGui().getTempsch());
+        //client.sendTempSchedule();
     }
 
     public void sendUpdate(String str) {
--- src/client/view/MainPanel.java
+++ src/client/view/MainPanel.java
@@ -17,6 +17,7 @@ public class MainPanel extends JPanel {
     private Controller controller;
     //private LightPanel lightPanel;
     private GUI gui;
+    private CurtainGui cgui;
     private BorderLayout layout;
 
     public MainPanel(Controller controller) {
@@ -32,14 +33,19 @@ public class MainPanel extends JPanel {
         setBorder(new CompoundBorder(border, margin));
         //lightPanel = new LightPanel(controller);
         gui=new GUI(controller);
-
+        cgui = new CurtainGui(controller);
         //add(lightPanel, BorderLayout.NORTH);
         add(gui);
+        add(cgui);
         //add(curtainPanel, BorderLayout.CENTER);
         //add(lockPanel, BorderLayout.SOUTH);
     }
 
     public GUI getGui(){
         return gui;
+    }
+
+    public CurtainGui getCurtainGui(){
+        return cgui;
     }
 }
